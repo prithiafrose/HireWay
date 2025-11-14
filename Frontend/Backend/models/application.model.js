752b2d1fcff415b7b233e6318994c8bc12645fb2
Prithi Afrose <prithi.afrose@example.com>
2025-11-15 01:13:56 +0600

    Remove Backend and duplicate Frontend folder

--- Frontend/Backend/models/application.model.js
+++
@@ -1,6 +0,0 @@
-import { DataTypes } from "sequelize";
-import sequelize from "./index.js";
-
-export const Application = sequelize.define("Application", {
-  status: { type: DataTypes.STRING, defaultValue: "pending" },
-});
